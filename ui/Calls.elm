dfadfa730d70adfecb9f8b10809a2483a2f2b984
Colin Woodbury <colingw@gmail.com>
2017-04-20 12:28:34 -0700

    (ui) Functions for backend calls

--- ui/Calls.elm
+++ ui/Calls.elm
@@ -1,11 +1,21 @@
-module Calls exposing (..)
+module Calls exposing
+    ( -- * Types
+      Person
+    , Block
+    , BlockSignin
+      -- * Json Encoding
+    , encodePerson, encodeBlock, encodeBSI
+      -- * API Calls
+    , register, signin, groups
+    )
 
 import Array as A
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
-import Json.Encode as J
+import Json.Decode as JD
+import Json.Encode as JE
 
 ---
 
@@ -16,29 +26,34 @@ type alias Person = { uuid    : Int
                     , city    : String
                     , company : String }
 
-encodePerson : Person -> J.Value
-encodePerson p = J.object [ ("uuid", J.int p.uuid)
-                          , ("fname", J.string p.fname)
-                          , ("lname", J.string p.lname)
-                          , ("city", J.string p.city)
-                          , ("company", J.string p.company) ]
+encodePerson : Person -> JE.Value
+encodePerson p = JE.object [ ("uuid",    JE.int p.uuid)
+                           , ("fname",   JE.string p.fname)
+                           , ("lname",   JE.string p.lname)
+                           , ("city",    JE.string p.city)
+                           , ("company", JE.string p.company) ]
 
 -- | A panel discussion time block.
 type Block = A | B | C
 
-encodeBlock : Block -> J.Value
-encodeBlock b = J.string <| toString b
+encodeBlock : Block -> JE.Value
+encodeBlock b = JE.string <| toString b
 
 -- | A group of attendees to sign in on the second day.
 type alias BlockSignin = { block : Block
                          , topic : String
                          , group : List Int }
 
-encodeBSI : BlockSignin -> J.Value
-encodeBSI bsi = J.object [ ("block", encodeBlock bsi.block)
-                         , ("topic", J.string bsi.topic)
-                         , ("group", J.array <| A.fromList <| List.map J.int bsi.group)
-                         ]
+encodeBSI : BlockSignin -> JE.Value
+encodeBSI bsi = JE.object [ ("block", encodeBlock bsi.block)
+                          , ("topic", JE.string bsi.topic)
+                          , ("group", JE.array <| A.fromList <| List.map JE.int bsi.group) ]
 
---getFoo : Http.Request String
---getFoo = Http.getString "http://localhost:8081/ping"
+register : String -> Person -> Http.Request String
+register host p = Http.post (host ++ "/register") (Http.jsonBody <| encodePerson p) JD.string
+
+signin : String -> Int -> Http.Request String
+signin host uuid = Http.post (host ++ "/signin") (Http.jsonBody <| JE.int uuid) JD.string
+
+groups : String -> BlockSignin -> Http.Request String
+groups host bsi = Http.post (host ++ "/groups") (Http.jsonBody <| encodeBSI bsi) <| JD.string
